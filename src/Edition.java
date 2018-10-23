082adbeb5d5335558df04be7319e259b069f6941
shenfeibit <shenfeibit@gmail.com>
2018-10-23 16:46:40 +0200

    nouveauPullPourTous

--- src/Edition.java
+++ src/Edition.java
@@ -4,13 +4,8 @@ public class Edition {
 	private int noEdition;
 	private Date dateDebER;
 	private Date dateFinER;
-<<<<<<< HEAD
 	private ArrayList<Etape> listEtape;
 	private ArrayList<Participant> listPart;
-=======
-	private ArrayList<Etape> listEtape = new ArrayList<Etape>();
->>>>>>> 92662dd176a918e4336da897c3692fd7d6da8cba
-
 	
 	public Edition(int noE, Date deb, Date fin) {
 		this.noEdition = noE;
@@ -18,10 +13,6 @@ public class Edition {
 		this.dateFinER = fin;
 		this.listEtape = new ArrayList<Etape>();
 		this.listPart = new ArrayList<Participant>();
-	}
-	
-	public ArrayList<Participant> getListPart() {
-		return listPart;
 	}
 
 	public void organiserEtape(Etape etape) {
@@ -34,6 +25,10 @@ public class Edition {
 	
 	public ArrayList<Etape> getListEtape() {
 		return listEtape;
+	}
+	
+	public ArrayList<Participant> getListPart() {
+		return listPart;
 	}
 
 }
