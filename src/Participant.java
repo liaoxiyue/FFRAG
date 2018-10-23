f5e57488cd1adb2696de917e01d99d509dcc3d9d
landylaura <44394762+landylaura@users.noreply.github.com>
2018-10-23 18:52:38 +0200

    dcsvf

--- src/Participant.java
+++ src/Participant.java
@@ -1,6 +1,10 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 public class Participant {
 
@@ -9,12 +13,15 @@ public class Participant {
     private Date tempsFinal;
     private Coureur coureur;
     private Vehicule vehicule;
+    private Edition edition;
     
-    public Participant(int numInscr, Date dateEnreg, Coureur c, Vehicule v) {
+    
+    public Participant(int numInscr, Date dateEnreg, Coureur c, Vehicule v, Edition e) {
         this.noInscription = numInscr;
         this.dateInscription = dateEnreg;
         this.coureur = c;
         this.vehicule = v;
+        this.edition = e;
         
     }
     
@@ -29,6 +36,20 @@ public class Participant {
     		prendreDepart=false;
     	}
     	return prendreDepart;
+    }
+    
+    public void setTempsFinal() throws ParseException {
+    	SimpleDateFormat dateformat = new SimpleDateFormat("hh:mm:ss");
+    	Calendar d1 = Calendar.getInstance();
+    	d1.setTime(dateformat.parse("00:00:00"));
+    	
+    	
+    	for(Etape e: this.edition.getListEtape()) {
+    		
+    		d1.add(Calendar.DATE,(int) e.getTabparticipants().get(this).getTempsEtape().getTime());
+    	}
+
+    	
     }
     
   
--- src/RunRallye.java
+++ src/RunRallye.java
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat;
 
 public class RunRallye {
 
@@ -7,6 +8,20 @@ public class RunRallye {
 
 		Voiture v1 = new Voiture("vme", 50);
 		System.out.println(v1.getCoef());
+	
+	SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+
+	Date d1, d2;
+	d1 = dateformat.parse("10/01/2018");
+	d2 = dateformat.parse("15/01/2018");
+
+	Edition e1 = new Edition(1,d1,d2 );
+	
+	Etape et1, et2;
+	et1 = new Etape(1, 400);
+	et2 = new Etape(2, 500);
+	
+	e1.
 	}
 
 }
