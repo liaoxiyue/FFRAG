9b45bfba6ccaec05195c64eaf9056b00c41246f2
Manuel <manuel.castillobarreiro@gmail.com>
2018-10-23 11:43:09 +0200

    création class participant

---
+++ src/Participant.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Participant {
+
+    private int NoInscription;
+    private Date DateInscription;
+    private float TempsFinal;
+
+    public void inscrire() {
+
+    }
+
+    public void prendreDepart() {
+
+    }
+}
