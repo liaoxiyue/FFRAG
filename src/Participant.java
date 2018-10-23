41cc2c0217589aef4bdfe0523e6286d2f2a15114
Manuel <manuel.castillobarreiro@gmail.com>
2018-10-23 18:03:56 +0200

    .

--- src/Participant.java
+++ src/Participant.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Participant {
 
     private int noInscription;
@@ -6,21 +7,30 @@ public class Participant {
     private float tempsFinal;
     private Coureur coureur;
     private Vehicule vehicule;
-    
-    public Participant(int numInscr, Date dateEnreg, Coureur c, Vehicule v) {
+    private Edition edition;
+
+    public Participant(int numInscr, Date dateEnreg, Coureur c, Vehicule v, Edition e) {
 
         this.noInscription = numInscr;
         this.dateInscription = dateEnreg;
         this.coureur = c;
         this.vehicule = v;
+        this.edition = e;
     }
 
     public boolean prendreDepart(Etape e) {
-    	boolean prendreDepart = true;
-   		if(e.getTabparticipants().get(this)==null) {
-    			prendreDepart=false;
-    		};
-    	return prendreDepart;
+        boolean prendreDepart = true;
+        if (e.getTabparticipants().get(this) == null) {
+            prendreDepart = false;
+        }
+        ;
+        return prendreDepart;
     }
-   
+
+    public float tempsFinal() {
+
+        float temps = 0;
+
+        return temps;
+    }
 }
