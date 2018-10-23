cf64fec354b6e933e62cdfdbcb9309376c863b4d
shenfeibit <shenfeibit@gmail.com>
2018-10-23 11:57:26 +0200

    coureurs change

--- src/Coureur.java
+++ src/Coureur.java
@@ -1,7 +1,13 @@
 import java.util.Date;
 
 public class Coureur {
-	private String nonCoureur;
+	private String nomCoureur;
 	private String prenomCoureur;
 	private Date dateNaissanceC;
+	
+	Coureur(String nom, String prenom, Date datene){
+		this.nomCoureur = nom;
+		this.prenomCoureur = prenom;
+		this.dateNaissanceC = datene;
+	}
 }
